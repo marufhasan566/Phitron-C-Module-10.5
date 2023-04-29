3ae08d554a77428dc0980c11b2afeb60f2126d27
marufhasan566 <marufhasan.mh1@gmail.com>
2023-04-29 23:55:56 +0600

    F_Way_Too_Long_Words

---
+++ F_Way_Too_Long_Words.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char c[100];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%s", c);
+        // printf("%d\n", strlen(c));
+        if (strlen(c) <= 10)
+        {
+            printf("%s\n", c);
+        }
+        else
+        {
+            printf("%c%d%c\n", c[0], strlen(c) - 2, c[strlen(c) - 1]);
+        }
+    }
+    return 0;
+}
